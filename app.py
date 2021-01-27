47661aa86b788f7e9143bd9456bfb5dd3eaa4954
macbookpro <tima05ta58@gmail.com>
2021-01-27 22:52:41 +0500

    update

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from aiogram import executor
 from loader import dp
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
+from utils.misc import logging
 
 
 async def on_startup(dispatcher):
@@ -10,4 +11,5 @@ async def on_startup(dispatcher):
 
 
 if __name__ == '__main__':
+    logging.info('Bot Started')
     executor.start_polling(dp, on_startup=on_startup)
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,12 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from loader import dp
+from loader import dp, bot
 
 
 @dp.message_handler(commands=['start'])
 async def bot_start(message: types.Message):
     await message.answer(f"Привет, {message.from_user.full_name}!")
+    await bot.send_photo(message.chat.id,
+                         photo='https://sun9-60.userapi.com/impg/c854124/v854124253/248c05/iz-LFXskFTc.jpg?size=1920x1920&quality=96&proxy=1&sign=810dd1b4b06802fa4508b411b2205afe&type=album',
+                         caption='Это его <code>template</code>')
